7970f0f5c8cabeec3eee92b6d4626d136f9b37d8
coton_chen <coton.chen@gmail.com>
2016-02-01 19:59:29 +0800

    201602011959

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,18 @@
-
 var app = app || {};
-app.init = function(){
-	this.bind_upload_event();
+app.init = function() {
+    this.bind_upload_event();
 };
 
-app.bind_upload_event = function(){
- 	$("#container").dropzone({
+app.bind_upload_event = function() {
+    $("#container").dropzone({
         url: "upload.php",
         maxFiles: 1,
         maxFilesize: 512,
         acceptedFiles: ".jpg,.png,.gif",
+        // uploadMultiple: true,
         init: function() {
-            this.on("success", function(file) {
+            this.on("success",
+            function(file) {
                 console.log("File " + file.name + "uploaded");
                 var uuid = file.xhr.responseText.trim();
                 $("#artimg").attr("src", "uploads/" + uuid);
@@ -20,7 +21,8 @@ app.bind_upload_event = function(){
 
                 app.generate_qrcode(url);
             });
-            this.on("removedfile", function(file) {
+            this.on("removedfile",
+            function(file) {
                 console.log("File " + file.name + "removed");
             });
         }
@@ -28,32 +30,38 @@ app.bind_upload_event = function(){
     });
 };
 
-app.generate_qrcode = function(url){
-	// $("#qrcode").qrcode({
-	// 	"render": "div",
-	//     "size": 200,
-	//     "color": "#3a3",
-	//     "text": url
-	// });
-	$("#qrcode").html("");
-    var qrcode = new QRCode("qrcode");
+app.generate_qrcode = function(url) {
+    // $("#qrcode").qrcode({
+    //  "render": "div",
+    //     "size": 200,
+    //     "color": "#3a3",
+    //     "text": url
+    // });
+    $("#qrcode").html("");
+    var qrcode = new QRCode("qrcode", {
+        width: 200,
+        height: 200
+    });
     qrcode.makeCode(url);
-};
 
+    setTimeout(function() {
+        $("#download").attr("href", $("#qrcode img").attr("src"));
+    },
+    1000);
+
+};
 
 /*-- tools
 ====================================================== */
-app.tools = function(){};
+app.tools = function() {};
 
-app.tools.getbaseurl = function(){
+app.tools.getbaseurl = function() {
     var url = window.location.href;
     return url.substring(0, url.lastIndexOf("/") + 1);
 };
 
+(function() {
 
-
-(function(){
-
-app.init();
+    app.init();
 
 })();
